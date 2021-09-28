cc41ce8a87cf32c3f6aab9363ce2bd9d594d6804
fuatguzel <fuatguzel754@gmail.com>
2021-09-28 20:20:55 +0300

    Use template engine ejs

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const ejs = require('ejs');
+const app = express();
 const PORT = 3000;
 
+// TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+// MIDDLEWARES
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
+  // 1
   // const exampleData = {
   //   id: 1,
   //   name: 'Name',
@@ -13,9 +19,17 @@ app.get('/', (req, res) => {
   // };
   // res.send(exampleData);
 
-  res.sendFile(path.resolve(__dirname, 'index.html'))
+  // 2
+  //res.sendFile(path.resolve(__dirname, 'index.html'))
+
+  // 3
+  res.render('index')
 
 });
+
+app.get('/add', (req, res) => {
+  res.render('add')
+})
 
 app.listen(PORT, (req, res) => {
   console.log(`Listening port ${PORT}...`);
