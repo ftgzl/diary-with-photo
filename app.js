fb4fa10e51fcca7ef5d8eff53f788a77d82ab1f9
fuatguzel <fuatguzel754@gmail.com>
2021-09-30 01:44:37 +0300

    Upload image and show on the page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload')
+const fs = require('fs');
 const Photo = require('./models/photo');
 const app = express();
 
@@ -20,6 +22,8 @@ app.use(express.static('public'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(fileUpload())
 
 
 app.get('/', async (req, res) => {
@@ -57,9 +61,23 @@ app.get('/add', (req, res) => {
 
 
 app.post('/photos', async (req, res) => {
-  //console.log(req.body);
-  await Photo.create(req.body)
-  res.redirect('/')
+  // witout image
+  //await Photo.create(req.body)
+
+  const uploadDir = 'public/uploads'
+
+  if(!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir)
+
+  let uploadedImage = req.files.image
+  let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadedImage.name
+    })
+    res.redirect('/')
+  })
 })
 
 
